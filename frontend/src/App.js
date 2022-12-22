17b39e66b9c5a0a8ebb51168f6a0defeb67df0d9
nambse <sefa.esendemir@gmail.com>
2022-12-22 14:12:47 +0300

    finished app

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import ProductList from "./pages/ProductList";
+import "./scss/style.scss";
+import ProductAdd from "./pages/ProductAdd";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<ProductList />} />
+          <Route path="/addproduct" element={<ProductAdd />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
